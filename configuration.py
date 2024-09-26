c8da6e63b2c2f48ed820264fc7946c5e1091f9b9
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2024-09-26 11:25:25 -0500

    Add project configuration and URLs

---
+++ configuration.py
@@ -0,0 +1,8 @@
+#URL_SERVICE = "copia la URL generada sin la barra inclinada al final"
+URL_SERVICE = "https://cnt-fce562f5-a504-4d60-96db-c26d1335a185.containerhub.tripleten-services.com"
+#DOC_PATH = "/docs/"
+DOC_PATH = "/docs/"
+
+CREATE_USER_PATH = "/api/v1/users/"
+
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
