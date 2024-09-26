5176acfad47f6bb274a11d3d18b08c861f9fe74f
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2024-09-26 11:25:04 -0500

    Add initial project configuration files

---
+++ sender_stand_request.py
@@ -0,0 +1,6 @@
+#URL_SERVICE = "copia la URL generada sin la barra inclinada al final"
+URL_SERVICE = "https://cnt-ad18a26e-81fa-4c68-a9fc-56ca3b62bf7b.containerhub.tripleten-services.com"
+#DOC_PATH = "/docs/"
+DOC_PATH = "/docs/"
+
+CREATE_USER_PATH = "/api/v1/users/"
